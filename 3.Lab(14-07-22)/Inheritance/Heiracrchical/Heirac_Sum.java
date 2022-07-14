a16f4199faf1e81e164b6f9de4d43bd0a3e77b09
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 18:02:16 +0530

    Add files via upload

---
+++ 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Sum.java
@@ -0,0 +1,16 @@
+package Revision;
+
+public class Heirac_Sum extends  Heirac_Data{
+
+	public void sum()
+	{
+		accept();
+		System.out.print("Sum : "+(a+b));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Heirac_Sum ob = new Heirac_Sum();
+		ob.sum();
+	}
+
+}
