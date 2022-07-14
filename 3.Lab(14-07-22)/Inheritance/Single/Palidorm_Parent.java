26ca40d7c78f7f7009cd690883be960abfe7e2fc
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 20:18:33 +0530

    Update Palidorm_Parent.java

--- 3.Lab(14-07-22)/Inheritance/Single/Palidorm_Parent.java
+++ 3.Lab(14-07-22)/Inheritance/Single/Palidorm_Parent.java
@@ -2,20 +2,28 @@ package Revision;
 
 import java.util.Scanner;
 
+// Parent class 
 public class Palidorm_Parent {
 	public void pali()
 	{
+		// global variable declaration
 		int n,r,temp,sum=0;
 		Scanner sc = new Scanner(System.in);
+		// Taking a no from user
 		System.out.print("Enter a no : ");
 		n = sc.nextInt();
+		
+		// original no is save in temp variable 
 		temp = n;
-		while(n>0)
+		// checking no is greater than zero
+		while(n>0)			
 		{
+			// logic to check no a palidrom or not
 			r = n%10;
 			sum = (sum*10)+r;
 			n = n/10;
 		}
+		// check temp is equal to sum or not
 		if(temp == sum)
 		{
 			System.out.println("This is Palidorm no : ");
