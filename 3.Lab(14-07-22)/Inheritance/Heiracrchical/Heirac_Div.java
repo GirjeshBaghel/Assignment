0e6aba80ba5a99eed48e94442b3d2f3fd8c57834
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 18:00:26 +0530

    Add files via upload

---
+++ 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Div.java
@@ -0,0 +1,14 @@
+package Revision;
+
+public class Heirac_Div extends Heirac_Data {
+	public void div()
+	{
+		accept();
+		System.out.print("Division : "+(a/b));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Heirac_Div ob = new Heirac_Div();
+		ob.div();
+	}
+}
