14b36c6251d6c9572a73485fc68e9b99eab79e94
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 17:59:52 +0530

    Create Heirac_Data.java

---
+++ 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Data.java
@@ -0,0 +1,16 @@
+package Revision;
+
+import java.util.Scanner;
+
+public class Heirac_Data {
+
+	Scanner sc = new Scanner(System.in);
+	int a,b,cal;
+	public void accept()
+	{
+		System.out.print("Enter the two no here :  ");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+	}
+}
