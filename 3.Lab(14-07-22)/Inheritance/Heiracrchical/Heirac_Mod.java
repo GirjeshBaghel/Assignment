41e5fbf46a41e4fc46fc072c42fe58dd41488564
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 18:01:09 +0530

    Add files via upload

---
+++ 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Mod.java
@@ -0,0 +1,15 @@
+package Revision;
+
+public class Heirac_Mod extends Heirac_Data {
+
+	public void mod()
+	{
+		accept();
+		System.out.print("Modulas : "+(a%b));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Heirac_Mod ob = new Heirac_Mod();
+		ob.mod();
+	}
+}
