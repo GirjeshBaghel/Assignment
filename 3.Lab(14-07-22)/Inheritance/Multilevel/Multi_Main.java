707c933bd3bb3d6335d1be618ee097fe476263b7
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 18:04:15 +0530

    Add files via upload

---
+++ 3.Lab(14-07-22)/Inheritance/Multilevel/Multi_Main.java
@@ -0,0 +1,13 @@
+package Revision;
+
+public class Multi_Main extends Multi_Pali {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Multi_Main ob = new Multi_Main();
+		//ob.pali();
+		ob.fibo();
+
+	}
+
+}
