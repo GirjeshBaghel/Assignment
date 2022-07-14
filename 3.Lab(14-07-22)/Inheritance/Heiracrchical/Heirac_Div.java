667426a959a2f652bca449d5708e7d226b709130
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 19:18:37 +0530

    Update Heirac_Div.java

--- 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Div.java
+++ 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Div.java
@@ -1,13 +1,18 @@
+// WAP where one class taking data and second class Sum ,class Mul,class Div,class Sub, class Mod. These classes taking input from data class
 package Revision;
 
+// Divison Class
 public class Heirac_Div extends Heirac_Data {
 	public void div()
 	{
+		// Taking input from main class
 		accept();
+		// Printing Division here 
 		System.out.print("Division : "+(a/b));
 	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// main method
+	public static void main(String[] args) 
+	{
 		Heirac_Div ob = new Heirac_Div();
 		ob.div();
 	}
