75f61182492f657b6fb2ac9f53c76021ec514c6b
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 20:20:18 +0530

    Update Palidorm_Child.java

--- 3.Lab(14-07-22)/Inheritance/Single/Palidorm_Child.java
+++ 3.Lab(14-07-22)/Inheritance/Single/Palidorm_Child.java
@@ -2,8 +2,10 @@ package Revision;
 
 public class Palidorm_Child  extends Palidorm_Parent{
 
+	// main method
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// calling the parent class method
 		Palidorm_Child ob = new Palidorm_Child();
 		ob.pali();
 	}
