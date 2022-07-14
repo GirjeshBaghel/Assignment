4da754110daef38ad24a6a55f6e4b49d2e83e75c
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 18:01:27 +0530

    Add files via upload

---
+++ 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Multi.java
@@ -0,0 +1,14 @@
+package Revision;
+
+public class Heirac_Multi extends  Heirac_Data{
+	public void mul()
+	{
+		accept();
+		System.out.print("Multiplication : "+(a*b));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Heirac_Multi ob = new Heirac_Multi();
+		ob.mul();
+	}
+}
