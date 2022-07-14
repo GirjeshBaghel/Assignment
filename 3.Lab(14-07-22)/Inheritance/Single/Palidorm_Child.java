d190f13ece91491ca28d031d2ecc56ef5cb33f04
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 18:06:49 +0530

    Add files via upload

---
+++ 3.Lab(14-07-22)/Inheritance/Single/Palidorm_Child.java
@@ -0,0 +1,11 @@
+package Revision;
+
+public class Palidorm_Child  extends Palidorm_Parent{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Palidorm_Child ob = new Palidorm_Child();
+		ob.pali();
+	}
+
+}
