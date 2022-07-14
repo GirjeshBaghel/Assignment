8ee7760be1279f6fc4ed4780f803336074fb5700
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 18:01:55 +0530

    Add files via upload

---
+++ 3.Lab(14-07-22)/Inheritance/Heiracrchical/Heirac_Sub.java
@@ -0,0 +1,17 @@
+package Revision;
+
+public class Heirac_Sub extends Heirac_Data {
+
+	public void sub()
+	{
+		accept();
+		System.out.print("Subtraction : "+(a-b));
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Heirac_Sub ob = new Heirac_Sub();
+		ob.sub();
+	}
+
+
+}
