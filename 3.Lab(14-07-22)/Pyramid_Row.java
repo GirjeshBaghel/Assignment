b2bf634f5d339a519aa8fe3d225b2ea345657aea
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 19:06:39 +0530

    Update Pyramid_Row.java

--- 3.Lab(14-07-22)/Pyramid_Row.java
+++ 3.Lab(14-07-22)/Pyramid_Row.java
@@ -4,40 +4,55 @@ package Revision;
 import java.util.Scanner;
 
 public class Pyramid_Row {
-
-	public static void main(String[] args)
-    {
-        Scanner sc = new Scanner(System.in);
- 
+	
+	// pymd method called by inside main method
+	public void pymd()
+	{
+		Scanner sc = new Scanner(System.in);
+		 
+		int i,j,row,count=1;
+		// Taking rows from user
         System.out.println("Enter the no of Rows here : ");
- 
-        int row = sc.nextInt();
-        int count = 1;
- 
+        
+        row = sc.nextInt();
+        count = 1;
+        
+        // Printing Pyramid
         System.out.println("Here Is Your Pyramid");
  
- 
-        for (int i = row; i > 0; i--)
+        for (i = row; i > 0; i--)
         {
- 
-            for (int j = 1; j <= i*2; j++)
+        	// loop for giving column spaces
+            for (j = 1; j <= i*2; j++)
             {
                 System.out.print(" ");
             }
-  
-            for (int j = 1; j <= count; j++)          
+            
+            // printing j increment value
+            for (j = 1; j <= count; j++)          
             {
                 System.out.print(j+" ");
             }
              
-            for (int j = count-1; j >= 1; j--)
+            //printing j decrement value
+            for (j = count-1; j >= 1; j--)
             {               
                 System.out.print(j+" ");            
             }                       
              
+            // Line break
             System.out.println();
   
             count++;
         }
+
+	}
+
+	// main method
+	public static void main(String[] args)
+    {
+		Pyramid_Row ob = new Pyramid_Row();
+		// calling pymd method 
+		ob.pymd();
     }
 }
