5240861ceb02c44c3a291f10f297e6d2c3598402
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-14 19:50:32 +0530

    Update Multi_Main.java

--- 3.Lab(14-07-22)/Inheritance/Multilevel/Multi_Main.java
+++ 3.Lab(14-07-22)/Inheritance/Multilevel/Multi_Main.java
@@ -1,11 +1,16 @@
+//WAP to create a class for Fibonacci series, second class Palindrom No , Third class use both the class property and display the output
 package Revision;
 
+// Main class taking all the properties of Fico or Pali class
 public class Multi_Main extends Multi_Pali {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// main method 
+	public static void main(String[] args) 
+	{
+
+		// calling the another class method
 		Multi_Main ob = new Multi_Main();
-		//ob.pali();
+		ob.pali();
 		ob.fibo();
 
 	}
