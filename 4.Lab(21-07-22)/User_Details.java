55e458b256755f611a41d31a9dbe6ca767927090
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-21 12:43:00 +0530

    Add files via upload

---
+++ 4.Lab(21-07-22)/User_Details.java
@@ -0,0 +1,39 @@
+package Revision;
+
+public class User_Details {
+
+	public String getBank() {
+		return bank;
+	}
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public long getAccount() {
+		return account;
+	}
+	public void setAccount(long account) {
+		this.account = account;
+	}
+	public long getPh() {
+		return ph;
+	}
+	public void setPh(long ph) {
+		this.ph = ph;
+	}
+	public int getPin() {
+		return pin;
+	}
+	public void setPin(int pin) {
+		this.pin = pin;
+	}
+	private String bank,name;
+	private long account,ph;	
+	private  int pin;
+	
+}
