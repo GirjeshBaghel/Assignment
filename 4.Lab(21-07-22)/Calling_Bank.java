1477feda7f4ed1d6ffcb2f5a6a28d95c3fd4e062
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-21 13:50:40 +0530

    Update Calling_Bank.java

--- 4.Lab(21-07-22)/Calling_Bank.java
+++ 4.Lab(21-07-22)/Calling_Bank.java
@@ -1,10 +1,12 @@
 package Revision;
 
-public class Calling_Bank extends Bank{
+// calling class
+public class Calling_Bank {
 
+	// main method 
 	public static void main(String[] args) {
+		// calling bank class method by creating a object
 		Bank ob = new Bank();
 		ob.details();
 	}
-
 }
