c6b067240e849f98816f90f6157b69ed57bd4ae9
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 14:19:19 +0530

    Update User_Excep.java

--- 5.Lab(28-07-22)/User_Details/User_Excep.java
+++ 5.Lab(28-07-22)/User_Details/User_Excep.java
@@ -1,10 +1,10 @@
 package Revision;
 
+// this is exception class that handle the another class exception 
 public class User_Excep  extends Exception{
 	
-	
-	User_Excep(String msg)
+	User_Excep(String msg) // constractor calling by the another class
 	{
-		super(msg);
+		super(msg); // passing the message for super class
 	}
 }
