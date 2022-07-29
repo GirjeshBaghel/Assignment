52f5f043308b845968eb5dfd0b75615770b6f155
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-07-29 14:11:30 +0530

    Update Main_Class.java

--- 5.Lab(28-07-22)/User_Details/Main_Class.java
+++ 5.Lab(28-07-22)/User_Details/Main_Class.java
@@ -2,11 +2,12 @@
 //the user Id lenght is greater than 8 and password is not matched and should only contain the string value.
 package Revision;
 
-public class Main_Class  extends Data_Class{
+public class Main_Class  extends Variable_Class{ // main class taking all the properties of Variable class
 	
 	
-	public void details()
+	public void details()	// details method calls inside the main method
 	{
+		// taking the details from user
 		System.out.println("Enter the Details Here.");
 		System.out.print("Name : ");
 		name = sc.next();
@@ -17,25 +18,25 @@ public class Main_Class  extends Data_Class{
 		System.out.print("Address : ");
 		add = sc.next();
 		
-		id();			
+		user_id(); // user_id mmethod calls			
 		
 	}
-	public void id()
+	public void user_id()
 	{
-		System.out.print("Id : ");
+		System.out.print("Id : "); // taking id from the user
 		try {
 			ide = sc.next();
 			setId(ide);
-			if(ide.length() != 6)
+			if(ide.length() != 6) // checking the length of id is 6 or not
 			{
-				throw new User_Excep("Enter the Valid Id here .");	
+				throw new User_Excep("Enter the Valid Id here ."); // if length is not equal to 6 then throw an exception	
 			}
 			else
 			{
-					password();		
+					user_pass(); // if all the things true then user_pass method calls		
 			}
 		}
-		catch(Exception e)
+		catch(Exception e) // if exception will be generate then catch block handle  
 		{
 			System.out.println(e);
 			System.out.println("The ID Shoud be maximun 6 length.");
@@ -43,59 +44,64 @@ public class Main_Class  extends Data_Class{
 		}
 		
 	}
-	public void password()
+	public void user_pass() // user_pass method call inside the useer_id method
 	{
+		// Password conditions here
 		System.out.print("1. A password must have at least eight characters.\n" +
                 "2. A password consists of only letters and digits.\n" +
                 "3. A password must contain at least two digits \n"); 
                s = sc.next();
                setPassword(s);
 			try {
-				if (is_Valid_Password(s)) {
-		            System.out.println("Password is valid: " + s);
-		            showing();
-		        } else {
-		        	throw new User_Excep("Not a valid password: ");
+				if (is_Valid_Password(s)) // is_Valid_Password method calls
+				{
+		            		System.out.println("Password is valid: " + s);
+		            		showing();
+		        	}	 
+				else {
+		        	throw new User_Excep("Not a valid password: "); // if is_Valid_Password method will carry false then throw a exception
 		           }
 			}
-			catch(Exception e1)
+			catch(Exception e1) 
 			{
 				System.out.println(e1);
 			}
 		
 	}	
-		public static boolean is_Valid_Password(String password) {
-
+		public static boolean is_Valid_Password(String password) { // this method checcking all the conditions of password
+		
+		// if password length is not equal to password_length then return a false
 	        if (password.length() < PASSWORD_LENGTH) return false;
 
 	        int charCount = 0;
 	        int numCount = 0;
+		// checking each character of a password by the loop	
 	        for (int i = 0; i < password.length(); i++) {
 
 	            char ch = password.charAt(i);
-
+			
 	            if (is_Numeric(ch)) numCount++;
 	            else if (is_Letter(ch)) charCount++;
 	            else return false;
 	        }
 
-
+		// return statement
 	        return (charCount >= 2 && numCount >= 2);
 	    }
-
+	    // checking the letter here
 	    public static boolean is_Letter(char ch) {
 	        ch = Character.toUpperCase(ch);
 	        return (ch >= 'A' && ch <= 'Z');
 	    }
 
-
+	    // checking the numeric value
 	    public static boolean is_Numeric(char ch) {
 
 	        return (ch >= '0' && ch <= '9');
 	    }
 
     
-	
+	// if all the things true then showing method will be call
 	public void showing()
 	{
 		System.out.println("Your Name : "+name);
@@ -106,9 +112,10 @@ public class Main_Class  extends Data_Class{
 		
 	}
 	
+	// main method
 	public static void main(String[] args)
 	{
-		Calling_Class ob = new Calling_Class();
+		Main_Class ob = new Main_Class();
 		ob.details();
 	}
 
