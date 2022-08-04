dee0b76ff7b77faa6970c6f0e61a9cf5e8cbe48b
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-04 12:54:38 +0530

    Update Reverse_No.java

--- 6.Lab(04-08-22)/Fibo_Series | Rev_No/Reverse_No.java
+++ 6.Lab(04-08-22)/Fibo_Series | Rev_No/Reverse_No.java
@@ -1,5 +1,5 @@
+/* Write a JAVA program which will generate the threads: To display 10 terms of Fibonacci series.- To display 1 to 10 in reverse order.*/
 package Revision;
-
 public class Reverse_No  extends FIbonacci_Series{
 	
 	// run method
