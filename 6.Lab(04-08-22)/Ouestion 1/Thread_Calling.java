5da310dd3ef98372d13190125bfec42d94a38aee
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-04 14:16:50 +0530

    Add files via upload

---
+++ 6.Lab(04-08-22)/Ouestion 1/Thread_Calling.java
@@ -0,0 +1,17 @@
+/*Write a program that creates 2 threads - each displaying a message (Pass the message as a parameter to the constructor). 
+The threads should display the messages continuously till the user presses any key.
+*/
+package Revision;
+ // the output of this program can't be terminte in exclipse by the key in cmd run properly
+public class Thread_Calling { 
+	
+	// main method
+	public static void main(String[] args)
+	{
+		Thread_Continue ob = new Thread_Continue("ACTIVE"); // passing the argument to the constructor
+		Thread_Continue ob1 = new Thread_Continue("UNACTIVE"); // passing the argument to the constructor
+		System.out.println("Display Output Here..");
+		ob.start(); // calling the run method
+		ob1.start(); // calling the run method
+	}
+}
