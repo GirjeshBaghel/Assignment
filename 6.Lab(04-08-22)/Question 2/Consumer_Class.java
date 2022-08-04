542212439a60b4cfab79d25c047f55a90e799ce3
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-04 14:38:02 +0530

    Update Consumer_Class.java

--- 6.Lab(04-08-22)/Question 2/Consumer_Class.java
+++ 6.Lab(04-08-22)/Question 2/Consumer_Class.java
@@ -2,23 +2,21 @@ package Revision;
 
 public class Consumer_Class  extends Thread {
 	
-	Problem_Class Shop;
-     int number,value = 0;
-    Consumer_Class( Problem_Class pc, int number)
-    {
-          Shop = pc;
-          this.number = number;
-    }
-    public void run()
-    {
-          for (int i = 0; i < 10; i++)
-          {
-				/*
-				 * try { sleep(1000); }catch(Exception e) { System.out.println(e); }
-				 */
-            
-                System.out.println("Consumed value " + this.number+ " got: " + value);
-          }
-    }
+	Problem_Class Shop;	//declared variable for using the class name.........
+	      int num,value = 0,i;  //declared varaible and data type......
+	     
+	      //create construtor.............
+	      public Consumer2(Problem_Class c, int number)
+	      {
+	            Shop = c;
+	            this.num = number;//use this keyword for use the same class variable...........
+	      }
+	      public void run()//create run method......
+	      {
+	            for ( i = 0; i < 10; i++)//using the for loop
+	            {
+	                  System.out.println("Consumed value " + this.num+ " got: " + value);//for print
+	            }
+	      }
 
 }
