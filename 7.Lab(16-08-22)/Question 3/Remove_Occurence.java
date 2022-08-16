03db4f5be5fcc7cbf4582133c896b2e35d952556
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-16 19:49:32 +0530

    Create Remove_Occurence.java

---
+++ 7.Lab(16-08-22)/Question 3/Remove_Occurence.java
@@ -0,0 +1,43 @@
+package String;
+
+import java.util.Scanner;
+
+public class Remove_Occurence {
+
+	Scanner sc = new Scanner(System.in);
+	String str;
+	char c;
+	int i,j,count;
+	
+	Remove_Occurence()
+	{
+		System.out.print("Enter a String here : ");
+		str = sc.nextLine();
+		System.out.print("Enter a Character : ");
+		c = sc.next().charAt(0);
+		str = str.toLowerCase();
+		char[] t = str.toCharArray();
+		for(i=j=0; i<str.length(); i++)
+		{
+			if(t[i] != c)
+			{
+				t[j++] = t[i];
+			}
+			else 
+				count++;
+		}
+		while(count>0)
+		{
+			t[j++] = '\0';
+	        count--;
+		}
+		System.out.println(t);
+		
+	}
+	
+	
+	public static void main(String... args)
+	{
+		new Remove_Occurence();
+	}
+}
