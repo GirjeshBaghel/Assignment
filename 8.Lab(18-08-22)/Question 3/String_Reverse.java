5d2915737781a1e553fb4706213c45c1d4da1187
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-20 21:58:31 +0530

    Update String_Reverse.java

--- 8.Lab(18-08-22)/Question 3/String_Reverse.java
+++ 8.Lab(18-08-22)/Question 3/String_Reverse.java
@@ -1,25 +1,26 @@
-//Write a program to reverse each word of the string.
+//Write a program to reverse each word of the string without using a reverse method.
 package String;
 import java.util.Scanner;
 
 public class String_Reverse {			
 	
-	static Scanner sc = new Scanner(System.in);			// global variable declaration
-	static StringBuffer str1 = new StringBuffer("");		// creating string buffer here 
-	static StringBuffer str2 = new StringBuffer("");
-	
-	 static void accept()						// accept method
+	Scanner sc = new Scanner(System.in);				// global variables are here
+	String str,st="";
+	char ch;
+	void accept()							// accept method that accept the string from user
 	{
-		System.out.print("Enter a First String : ");		// taking a string from user
-		str1.append(sc.nextLine());
-		System.out.print("Enter a Second  String : ");		// taking a another string from user
-		str2.append(sc.nextLine());
-				
+		System.out.print("Enter a String : ");
+		 str = sc.nextLine();
 	}
-	void reverse()							// reverse method
+	void reverse()							// reverse method that reverse a string here
 	{
-		accept();						// calling a reverse method
-		System.out.println("Reverse String are : "+str1.reverse());	
+		accept();						// calls a accept methods
+		for(int i=0; i<str.length(); i++)			// loop running for string length
+		{
+			ch = str.charAt(i);				// convert a string index whole value in char
+			st = ch+st;					// concat a char with string
+		}
+		System.out.print(st);	
 	}
 
 	public static void main(String[] args) {			// main method
