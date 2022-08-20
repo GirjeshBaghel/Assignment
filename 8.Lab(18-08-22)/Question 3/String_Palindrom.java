5ed52c93084a56765a513021137bc22fc2ec42c0
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-08-20 19:02:22 +0530

    Update String_Palindrom.java

--- 8.Lab(18-08-22)/Question 3/String_Palindrom.java
+++ 8.Lab(18-08-22)/Question 3/String_Palindrom.java
@@ -3,34 +3,34 @@ package String;
 import java.util.Scanner;
 
 public class String_Palindrom {
-	Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);					// global declaration
 	String st,rev="";
 	char ch;
 	
-	public void accept()
+	public void accept()							// accept the string from user
 	{
 		System.out.print("Enter a String : ");
 		st = sc.nextLine();
 		st = st.toLowerCase();
-		for(int i=0; i<st.length(); i++)
+		for(int i=0; i<st.length(); i++)				// loop running for the string length
 		{
-			ch = st.charAt(i);
-			rev = ch+rev;
+			ch = st.charAt(i);					// storing a string into a char
+			rev = ch+rev;									
 		}
 		
-		if(st.equals(rev))
+		if(st.equals(rev))						// checking a reverse string is equal to a normal string
 		{
-			System.out.println("It is Palindrom String : "+rev);
+			System.out.println("It is Palindrome String : "+rev);
 		}
 		else
-			System.out.println("Not a Palindrom String : "+st);
+			System.out.println("Not a Palindrome String : "+st);
 				
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) {				// main method
 		
-		String_Palindrom ob = new String_Palindrom();
-		ob.accept();
+		String_Palindrom ob = new String_Palindrom();			// creating a object 
+		ob.accept();							// calls a accept method
 	}
 
 }
