e0a24644071e007d03ea872e85e681398e592bb0
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-09-02 13:12:20 +0530

    Update Chained_Exception.java

--- 10.Lab(02-09-22)/Question 1/Chained_Exception.java
+++ 10.Lab(02-09-22)/Question 1/Chained_Exception.java
@@ -4,37 +4,37 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Chained_Exception {
-	Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);						// class variable
 	int a,b;
 	
-	void accept()
+	void accept()									// accept method that accept the value 
 	{
 		System.out.print("Enter A value : ");
-		a = sc.nextInt();
+		a = sc.nextInt();							// storing a value
 		System.out.print("Enter B value : ");
-		b = sc.nextInt();
-		try {
-			if(b==0)
+		b = sc.nextInt();							// storing  b value
+		try {									// try block to handle the exception
+			if(b==0)							// if b =0 then throw exception
 			{
-				ArithmeticException ae = new ArithmeticException();
-				ae.initCause(new IOException());
-				throw ae;
+			    ArithmeticException ae = new ArithmeticException();		// creating a exception
+			    ae.initCause(new IOException());				// object of arithmetic exception creaing a another exception
+			    throw ae;
 			}
 			else
-				System.out.println("Division : "+a/b);
+				System.out.println("Division : "+a/b);			// if b !=0 then divide the value
 		}
-		catch(ArithmeticException ae)
+		catch(ArithmeticException ae)						// catch the exception
 		{
-			System.out.println("Caught : "+ae);
-			System.out.println("Actual : "+ae.getCause());
+			System.out.println("Caught : "+ae);				// print the caught exception
+			System.out.println("Actual : "+ae.getCause());			// print the actual exception
 		}
 		
 	}
 
-	public static void main(String[] args) 
+	public static void main(String[] args) 						// main method
 	{
-		Chained_Exception ob =new Chained_Exception();
-		ob.accept();
+		Chained_Exception ob =new Chained_Exception();				// creating a object
+		ob.accept();								// calling a accept method
 	}
 
 }
