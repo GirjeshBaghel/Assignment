423a7c5df9a57dfefba70c4ba501155d8b569917
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-10-01 13:21:34 +0530

    Update CP.java

--- 13.Lab(30-09-22)/Question 2/CP.java
+++ 13.Lab(30-09-22)/Question 2/CP.java
@@ -2,14 +2,14 @@ package JDBC_Program;
 
 import java.sql.*;
 
-public class CP {
+public class CP {								// connection creating file
 	static Connection con;
 	public static Connection createC() throws Exception
 	{
-		String url = "jdbc:mysql://localhost:3306/student_manager";
+		String url = "jdbc:mysql://localhost:3306/student_manager";		// url of database
 		String user = "root";
 		String pass = "12345";
 		con = DriverManager.getConnection(url,user,pass);
-		return con;
+		return con;								// return a connection to the method
 	}
 }
