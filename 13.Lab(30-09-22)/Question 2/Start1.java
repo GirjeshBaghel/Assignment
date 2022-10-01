f9fe6fe4ea6a3a5fe899fbd14c70cc01c174dd7a
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2022-10-01 13:27:36 +0530

    Update Start1.java

--- 13.Lab(30-09-22)/Question 2/Start1.java
+++ 13.Lab(30-09-22)/Question 2/Start1.java
@@ -2,31 +2,31 @@ package JDBC_Program;
 
 import java.util.Scanner;
 
-public class Start1  {
+public class Start1  {							// start class that start the program
 	int c;
 	char ans;
 	String name,phone,city;
-	Scanner sc = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);				// global variables
 	
 	
-	public static void main(String[] args)  throws Exception{
+	public static void main(String[] args)  throws Exception{	// main method that call the start method
 		Start1 ob = new Start1();
 		ob.start();
 		
 	}
-	public void addstd() throws Exception
+	public void addstd() throws Exception				// add student method that add the data of student
 	{
 		System.out.print("Enter Student Name : ");
-		name  = sc.next();
+		name  = sc.next();					// taking the name 
 		
 		System.out.print("Enter Student Phone No  : ");
-		phone = sc.next();
+		phone = sc.next();					// taking the Phone no
 		 
-		 System.out.print("Enter Student City : ");
-		 city = sc.next();
+		 System.out.print("Enter Student City : ");		
+		 city = sc.next();					// taking the city name
 		 
-		 Student st = new Student(name,phone,city);
-		boolean ans = StudentDao.insertStudentToDB(st);
+		 Student st = new Student(name,phone,city);		// passing to the Student Constructor
+		boolean ans = StudentDao.insertStudentToDB(st);		// pass to the Student insert method
 		if(ans)
 		{
 			System.out.println("Successfully Added ..");
@@ -36,11 +36,11 @@ public class Start1  {
 		
 		 System.out.println(st);		
 	}
-	public void delstd() throws Exception
+	public void delstd() throws Exception					// delete student data method
 	{
 		System.out.print("Enter Student Id to Delete Data : ");
-		int d_id = sc.nextInt();
-		boolean ans =StudentDao.deleteStudentToDB(d_id);
+		int d_id = sc.nextInt();					// taking the student id
+		boolean ans =StudentDao.deleteStudentToDB(d_id);		// pass that id to the student class 
 		if(ans)
 		{
 			System.out.println("Successfully Added ..");
@@ -49,33 +49,33 @@ public class Start1  {
 			System.out.println("Something Went Wrong try again.");	
 	}
 	
-	public void start() throws Exception
+	public void start() throws Exception					// start method that the program
 	{
 		System.out.println("Welcome to Student Manager App");
 		do
 		{
-			System.out.println("PRESS 1 : ADD Student Data");
+			System.out.println("PRESS 1 : ADD Student Data");		// printing these lines of code
 			System.out.println("PRESS 2 : DELETE Student Data");
 			System.out.println("PRESS 3 : DISPLAY Student Data");
 			System.out.println("PRESS 4 : Update Student Data");
 			System.out.print("\nPlease Enter your Choice :" );
 			
 			 c = sc.nextInt();
-			switch(c)
+			switch(c)							// switch case to switch the conditions
 			{
 				case 1:
-					addstd();
+					addstd();					// call add methods
 					break;
 				
-				case 2:
-					delstd();
+				case 2:	
+					delstd();					// call to delete method
 					break;
 					
 				case 3:
-					StudentDao.showAllStudent();
+					StudentDao.showAllStudent();			// calls show method that are make in Student Class
 					break;
 				case 4:
-					StudentDao.updateStudent();
+					StudentDao.updateStudent();			// update method call that are make in Student Class
 					break;
 				default:
 					break;
